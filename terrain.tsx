36fb2981c921744f31fc86923051bec954ac16eb
Oliver Schlösser <oschl@yahoo.com>
2020-12-19 14:20:15 +0100

    First commit

---
+++ terrain.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="terrain" tilewidth="32" tileheight="32" tilecount="483" columns="21">
+ <image source="terrain.png" width="672" height="736"/>
+ <tile id="322">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
